f96dd892ac9a5348b4cbc998154ab95a2c6e8f35
Albert Boakye <noblejack27@gmail.com>
2021-10-08 19:40:58 -0700

    header file added with convertions

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	/*calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+
+/**
+ *void prototypes()
+ *int prototypes()
+ */
+
+int _printf(const char *format, ...);
+
+
+
+#endif /*MAIN_H_*/
